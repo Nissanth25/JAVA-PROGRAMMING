0803969673be54b4bb8f45318c1ec82f794b44d8
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-11-29 14:38:55 +0530

    4.java

--- 4.java
+++ 4.java
@@ -6,10 +6,8 @@ public class Solution {
         System.out.println("================================"); 
         
         for (int i = 0; i < 3; i++) {                                     
-            String s1 = sc.next();  // Read a string
-            int x = sc.nextInt();   // Read an integer
-            
-            // Format and print the output
+            String s1 = sc.next();  
+            int x = sc.nextInt();   
             System.out.printf("%-15s%03d%n", s1, x);
         }
         
