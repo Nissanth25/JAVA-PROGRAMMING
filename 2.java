dc6c7d3e078b974633c83e65611c28084254ed6d
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-11-29 14:36:07 +0530

    2.java

--- 2.java
+++ 2.java
@@ -10,18 +10,15 @@ public class Solution {
     public static void main(String[] args) throws IOException {  
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
         
-        // Reading input
         System.out.println("Enter three integers separated by spaces:");
         String[] input = reader.readLine().split(" ");
         int a = Integer.parseInt(input[0]);
         int b = Integer.parseInt(input[1]);
         int c = Integer.parseInt(input[2]);
 
-        // Example logic: Calculate sum and product
         int sum = a + b + c;
         int product = a * b * c;
 
-        // Output results
         System.out.println("Sum: " + sum);
         System.out.println("Product: " + product);
     }  
