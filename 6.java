169e42f37b3b33e5293030e1bbc47c0b1b481e5c
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-11-29 14:41:15 +0530

    6.java

--- 6.java
+++ 6.java
@@ -4,16 +4,15 @@ import java.io.*;
 class Solution {     
     public static void main(String []argh) {         
         Scanner in = new Scanner(System.in);         
-        int t = in.nextInt();  // Number of test cases
+        int t = in.nextInt();  
 
         for (int i = 0; i < t; i++) {                 
-            int a = in.nextInt();  // First integer
-            int b = in.nextInt();  // Second integer
+            int a = in.nextInt();  
+            int b = in.nextInt(); 
             
-            // Example logic: Sum of a and b
             int sum = a + b;
             System.out.println("Case #" + (i + 1) + ": " + sum);
         }                                                                                                                       
-        in.close();  // Close the scanner                                                                                                                             
+        in.close(); 
     }      
 }
