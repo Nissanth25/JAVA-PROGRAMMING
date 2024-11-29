b468666bf4ab0209a8adb16b40c5b586f7839583
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2024-11-29 14:38:26 +0530

    3.java

--- 3.java
+++ 3.java
@@ -4,12 +4,10 @@ public class Solution {
     public static void main(String[] args) {     
         Scanner scan = new Scanner(System.in);                  
         
-        // Reading inputs
-        int i = Integer.parseInt(scan.nextLine());  // Read an integer
-        double d = Double.parseDouble(scan.nextLine());  // Read a double
-        String s = scan.nextLine();  // Read a string (remaining input)
-        
-        // Output results
+        int i = Integer.parseInt(scan.nextLine());  
+        double d = Double.parseDouble(scan.nextLine());  
+        String s = scan.nextLine();  
+       
         System.out.println("Integer: " + i);
         System.out.println("Double: " + d);
         System.out.println("String: " + s);
